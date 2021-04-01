d61fd60fca537ced5e944199b734e1d2ce62af60
Thales Gomes <thalessud@gmail.com>
2021-04-01 11:56:15 +0100

    bank show page

--- app/controllers/banks_controller.rb
+++ app/controllers/banks_controller.rb
@@ -1,4 +1,8 @@
 class BanksController < ApplicationController
+  def show
+    @bank = Bank.find(params[:id])
+  end
+
   def new
     @user = User.find(current_user.id)
     @bank = Bank.new
@@ -7,7 +11,7 @@ class BanksController < ApplicationController
   def create
     @bank = Bank.new(bank_params)
     if @bank.save
-      redirect_to root_path, notice: 'Bank was successfully created'
+      redirect_to bank_path(@bank), notice: 'Bank was successfully created'
     else
       render :new
     end
