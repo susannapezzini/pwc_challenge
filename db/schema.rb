4c0bb5ffa721137c482fc6b71a5236c1a658713b
Giuseppe Forgione <giuseppeforgione11@gmail.com>
2021-04-01 12:02:54 +0100

    url field removed from banks

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_30_182000) do
+ActiveRecord::Schema.define(version: 2021_04_01_110219) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "banks", force: :cascade do |t|
     t.string "name", null: false
-    t.string "website", null: false
     t.string "address"
     t.string "country"
     t.datetime "created_at", precision: 6, null: false
