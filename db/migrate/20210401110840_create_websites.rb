f3241d9de42225ceecc8ab62e566e1bbd89d6b72
Giuseppe Forgione <giuseppeforgione11@gmail.com>
2021-04-01 12:09:24 +0100

    website table created

---
+++ db/migrate/20210401110840_create_websites.rb
@@ -0,0 +1,10 @@
+class CreateWebsites < ActiveRecord::Migration[6.1]
+  def change
+    create_table :websites do |t|
+      t.string :url
+      t.references :bank, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
