713e7025eb590b2f473d0cd160abbfb05de62161
Giuseppe Forgione <giuseppeforgione11@gmail.com>
2021-04-05 13:01:32 +0100

    fee controller edit and update

--- app/controllers/prices_controller.rb
+++ app/controllers/prices_controller.rb
@@ -1,12 +1,10 @@
 class PricesController < ApplicationController
+  before_action :fetch_price, only: [:edit, :update]
 
   def edit
-    @price = Price.find(params[:id])
   end
 
   def update
-    @price = Price.find(params[:id])
-
     if @price.update(price_params)
       redirect_to subproduct_path(@price.subproduct), notice: 'Price was successfully updated'
     else
@@ -19,4 +17,7 @@ class PricesController < ApplicationController
       params.require(:price).permit(:fee_id, :subproduct_id, :document, :name, :amount, :category, :tax_category, :tax, :tax_amount, :status)
     end
 
+    def fetch_price
+      @price = Price.find(params[:id])
+    end
 end
