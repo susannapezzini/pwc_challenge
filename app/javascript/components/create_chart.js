b02e7b8024a46f15e6dae13062aa0a6f21e71a55
Thales Gomes <thalessud@gmail.com>
2021-04-06 11:19:18 +0100

    improvement on readability and helper func implemented

--- app/javascript/components/create_chart.js
+++ app/javascript/components/create_chart.js
@@ -1,13 +1,12 @@
 const ctx = document.getElementById('compareChart');
-
-if (ctx) {
+const createChart = (ctx, bankName1, price1, bankName2, price2) => {
   const myChart = new Chart(ctx, {
     type: 'bar',
     data: {
-      labels: [myBankName, otherBankName],
+      labels: [bankName1, bankName2],
       datasets: [{
-        label: '# of Votes',
-        data: [myPrice, otherPrice],
+        label: 'Price Comparison',
+        data: [price1, price2],
         backgroundColor: ['rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(255, 206, 86, 0.2)', 'rgba(75, 192, 192, 0.2)', 'rgba(153, 102, 255, 0.2)', 'rgba(255, 159, 64, 0.2)'],
         borderColor: ['rgba(255, 99, 132, 1)', 'rgba(54, 162, 235, 1)', 'rgba(255, 206, 86, 1)', 'rgba(75, 192, 192, 1)', 'rgba(153, 102, 255, 1)', 'rgba(255, 159, 64, 1)'],
         borderWidth: 1
@@ -24,3 +23,5 @@ if (ctx) {
     }
   });
 }
+
+export default createChart
